a20820797bca6ec4fe0a170770f4802c965405a1
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-08-13 16:35:57 -0300

    :sparkles: feat: Adicionando controller do pedido específico

--- src/controllers/pedidos-controller.js
+++ src/controllers/pedidos-controller.js
@@ -1,4 +1,4 @@
-import { getPedidos } from "../models/pedidos-models.js";
+import { getPedidos, getPedido } from "../models/pedidos-models.js";
 
 
 export const listarPedidos = async (req, res) => {
@@ -11,4 +11,18 @@ export const listarPedidos = async (req, res) => {
             "erro" : "true"
         });
    }
+}
+
+export const listarPedido = async (req, res) => {
+    const id = req.params.id
+
+    try {
+        const Pedido = await getPedido(id)
+        res.status(200).json({Pedido})
+    } catch (error) {
+        res.status(400).json({
+            "msg" : error.message,
+            "erro" : "true"
+        });
+    }
 }
