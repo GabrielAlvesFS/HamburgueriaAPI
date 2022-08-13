225115681f8dee9f1bb10ffd225c40f7c5ccc9c3
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-08-13 19:06:26 -0300

    :recycle: refactor: removendo chaves

--- src/controllers/pedidos-controller.js
+++ src/controllers/pedidos-controller.js
@@ -4,7 +4,7 @@ import { getPedidos, getPedido } from "../models/pedidos-models.js";
 export const listarPedidos = async (req, res) => {
    try {
         const Pedidos = await getPedidos();
-        res.status(200).json({Pedidos})
+        res.status(200).json(Pedidos)
    } catch (error) {
         res.status(400).json({
             "msg" : error.message,
@@ -18,7 +18,7 @@ export const listarPedido = async (req, res) => {
 
     try {
         const Pedido = await getPedido(id)
-        res.status(200).json({Pedido})
+        res.status(200).json(Pedido)
     } catch (error) {
         res.status(400).json({
             "msg" : error.message,
