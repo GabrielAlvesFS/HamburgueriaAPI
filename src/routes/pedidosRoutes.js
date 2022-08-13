0cd8a13187822173087e5c1c667faaa7fde306de
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-08-13 16:36:42 -0300

    :sparkles: feat: Adicionando um GET na rota de pedidos

--- src/routes/pedidosRoutes.js
+++ src/routes/pedidosRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { listarPedidos } from "../controllers/pedidos-controller.js";
+import { listarPedidos, listarPedido } from "../controllers/pedidos-controller.js";
 
 const router = express.Router();
 
 router
     .get("/pedidos", listarPedidos)
+    .get("/pedidos/:id", listarPedido)
     
 export default router;   
