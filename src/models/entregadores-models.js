a0225cec49652b8609538cfa253c7e22977dad29
MagnusBittencourt <magnusbittencourt@gmail.com>
2022-08-13 16:46:05 -0300

    :sparkles: feat: criando a model de entregadores

---
+++ src/models/entregadores-models.js
@@ -0,0 +1,8 @@
+class Entregadores {
+    constructor(id, cpf, nome, telefone){
+        this.cpf = cpf
+        this.nome = nome
+        this. telefone = telefone
+    }
+}
+export default Entregadores
