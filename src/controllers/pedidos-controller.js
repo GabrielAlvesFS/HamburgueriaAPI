93cbd402b60547ab190fe372d296d3835aab4977
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-08-14 01:00:14 -0300

    :sparkles: feat: Adicionando controller para atualizar um pedido

--- src/controllers/pedidos-controller.js
+++ src/controllers/pedidos-controller.js
@@ -1,4 +1,4 @@
-import { Pedidos, getPedidos, getPedido, postPedido } from "../models/pedidos-models.js";
+import { Pedidos, getPedidos, getPedido, postPedido, putPedido } from "../models/pedidos-models.js";
 
 
 export const listarPedidos = async (req, res) => {
@@ -32,9 +32,26 @@ export const cadastrarPedido = async (req, res) => {
     const dataP = new Pedidos(cliente_id, 0, data_pedido, metodo_pagamento)
 
     try {
-        console.log(dataP)
         const newPedido = await postPedido(dataP) 
         res.status(201).send(newPedido)
+    } catch (error) {
+        res.status(400).json({
+            "msg" : error.message,
+            "erro" : "true"
+        });
+    }
+}
+
+export const alterarPedido = async (req, res) => {
+    const id = +req.params.id
+    const {entregador_id, status_pedido, metodo_pagamento} = req.body
+    const dataP = new Pedidos(entregador_id, status_pedido, metodo_pagamento)
+    // console.log(datap)
+    // const body = req.body
+
+    try {
+        const updPedido = await putPedido(id, dataP)
+        res.status(200).json(updPedido)
     } catch (error) {
         res.status(400).json({
             "msg" : error.message,
