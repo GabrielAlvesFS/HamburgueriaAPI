4b8aa22370ba27b705f533231f4d654890c28ce7
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-08-14 18:01:45 -0300

    :sparkles: feat: Adicionando controller que lista itens de um pedido específico

--- src/controllers/itemPedido-controller.js
+++ src/controllers/itemPedido-controller.js
@@ -1,8 +1,22 @@
-import { getItensPedido } from "../models/itemPedido-models.js";
+import { getItensPedidos, getItensPedido } from "../models/itemPedido-models.js";
+
+export const listarItensPedidos = async (req, res) => {
+    try {
+        const ItensPedidos = await getItensPedidos()
+        res.status(200).json(ItensPedidos)
+    } catch (error) {
+        res.status(400).json({
+            "msg" : error.message,
+            "erro" : "true"
+        });
+    }
+}
 
 export const listarItensPedido = async (req, res) => {
+    const pedidoId = req.params.pedido_id
+
     try {
-        const ItensPedido = await getItensPedido()
+        const ItensPedido = await getItensPedido(pedidoId)
         res.status(200).json(ItensPedido)
     } catch (error) {
         res.status(400).json({
