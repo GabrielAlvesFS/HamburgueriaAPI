62628fc23c6817a754ba1e664a3766f83919e99d
MagnusBittencourt <magnusbittencourt@gmail.com>
2022-08-14 21:52:21 -0300

    :sparkles: feat: Adicionando models dos entregadores

--- src/models/entregadores-models.js
+++ src/models/entregadores-models.js
@@ -1,8 +1,41 @@
-class Entregadores {
-    constructor(id, cpf, nome, telefone){
-        this.cpf = cpf
+import { listarEntregadores, listarEntregador, criarEntregador } from "../DAO/entregadoresDAO.js"
+
+
+export class Entregadores {
+    constructor( nome, cpf, telefone){
         this.nome = nome
-        this. telefone = telefone
+        this.cpf = cpf
+        this.telefone = telefone
     }
 }
-export default Entregadores
+
+export const getEntregadores = async () => {
+    try {
+        const dados = await listarEntregadores()
+        if (!dados) throw new Error("Não foi possível encontrar os entregadores")
+        return dados
+    } catch (error) {
+        throw error
+    }
+} 
+
+export const getEntregador = async (id) => {
+    try {
+        const dados = await listarEntregador(id)
+        if(!dados) throw new Error("Não foi possível encontrar o entregador")
+        return dados
+    } catch (error) {
+        throw error
+    }
+}
+
+
+export const postEntregador =  async (entregador) => {
+    try {
+        const resposta = await criarEntregador(entregador)
+        if(!resposta) throw new Error("Não foi possível cadastrar o entregador")
+        return resposta
+    } catch (error) {
+        throw error
+    }
+}
