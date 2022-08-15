116bfaf8fef6192b8880f31978c3a11703a0080c
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-08-15 20:22:03 -0300

    :sparkles: feat: Adicionando controller que remove o item do pedido

--- src/controllers/itemPedido-controller.js
+++ src/controllers/itemPedido-controller.js
@@ -1,4 +1,4 @@
-import { ItemPedido, getItensPedidos, getItensPedido, postItensPedido, somaPedido, putItensPedido } from "../models/itemPedido-models.js";
+import { ItemPedido, getItensPedidos, getItensPedido, postItensPedido, somaPedido, putItensPedido, deleteItensPedido } from "../models/itemPedido-models.js";
 import { putValorPedido } from "../models/pedidos-models.js";
 
 export const listarItensPedidos = async (req, res) => {
@@ -54,16 +54,17 @@ export const atualizarItensPedido = async (req, res) => {
     const {item_id, quantidade_itens} = req.body
     const pedido_id = +req.params.pedido_id
     const dataP = new ItemPedido(pedido_id, item_id, quantidade_itens)
-    const id = +req.params.id
+    const itemPedido_id = +req.params.id
 
     try {
-        const newItemPedido = await putItensPedido(dataP, id) 
-        res.status(201).send(newItemPedido)
+        const newItemPedido = await putItensPedido(dataP, itemPedido_id) 
+        res.status(200).send(newItemPedido)
+        
         // totalPedido -> total dos itens do pedido especifico é atualizado quando cadastrado um novo pedido
         let totalPedido = await somaPedido(pedido_id)
         totalPedido = Object.values(totalPedido[0])
         totalPedido = totalPedido[0]
-        console.log(totalPedido)
+    
         //atualizando o valor total do pedido na tabela de pedidos com o id do pedido específico
         const updPedido = await putValorPedido(totalPedido, pedido_id)
     } catch (error) {
@@ -71,5 +72,25 @@ export const atualizarItensPedido = async (req, res) => {
             "msg" : error.message,
             "erro" : "true"
         });
+    }
+}
+
+export const removerItensPedido = async (req, res) => {
+    const pedido_id = +req.params.pedido_id
+    const itemPedido_id = +req.params.id
+
+    try {
+        const delItemPedido = await deleteItensPedido(itemPedido_id) 
+        res.status(200).send(delItemPedido)
+
+        // totalPedido -> total dos itens do pedido especifico é atualizado quando cadastrado um novo pedido
+        let totalPedido = await somaPedido(pedido_id)
+        totalPedido = Object.values(totalPedido[0])
+        totalPedido = totalPedido[0]
+
+        //atualizando o valor total do pedido na tabela de pedidos com o id do pedido específico
+        const updPedido = await putValorPedido(totalPedido, pedido_id)
+    } catch (error) {
+        throw error
     }
 }
