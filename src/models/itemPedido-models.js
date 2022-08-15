e9b4e6843a0961ee7341ba4808f72f2aa071b9ce
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-08-15 02:47:34 -0300

    :sparkles: feat: Adicionando a model para cadastrar o pedido e a model de soma do pedido

--- src/models/itemPedido-models.js
+++ src/models/itemPedido-models.js
@@ -1,17 +1,17 @@
-import { listarItensPedidos, listarItensPedido } from "../DAO/itemPedidoDAO.js";
+import { listarItensPedidos, listarItensPedido, cadastrarItemPedido, somaItensPedido } from "../DAO/itemPedidoDAO.js";
 
 export class ItemPedido {
     constructor(pedido_id, item_id, quantidade_itens){
         this.pedido_id = pedido_id;
         this.item_id = item_id;
-        this,quantidade_itens = quantidade_itens;
+        this.quantidade_itens = quantidade_itens;
     }
 }
 
 export const getItensPedidos = async () => {
     try {
         const dados = await listarItensPedidos()
-        if (!dados) throw new Error("Não foi possível encontrar os itens do pedido")
+        if (!dados) throw new Error("Não foi possível encontrar os itens dos pedidos")
         return dados
     } catch (error) {
         throw(error)
@@ -25,5 +25,25 @@ export const getItensPedido = async (pedido_id) => {
         return dados
     } catch (error) {
         throw(error)
+    }
+}
+
+export const postItensPedido = async (itemPedido) => {
+    try {
+        const newItemPedido = await cadastrarItemPedido(itemPedido)
+        if (!newItemPedido) throw new Error("Não foi possível cadastrar o Item do pedido")
+        return newItemPedido
+    } catch (error) {
+        throw error
+    }
+}
+
+export const somaPedido = async (pedido_id) => {
+    try {
+        const soma = await somaItensPedido(pedido_id)
+        if (!soma) throw new Error("Não foi possível cadastrar o Item do pedido")
+        return soma
+    } catch (error) {
+        throw error
     }
 }
