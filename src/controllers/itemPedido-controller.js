89612b6928cdf232092835b9ec1733de6b7a2d85
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-08-15 02:48:33 -0300

    :sparkles: feat: Adicionando controller que cadastra um novo pedido e que atualiza o valor do pedido

--- src/controllers/itemPedido-controller.js
+++ src/controllers/itemPedido-controller.js
@@ -1,4 +1,5 @@
-import { getItensPedidos, getItensPedido } from "../models/itemPedido-models.js";
+import { ItemPedido, getItensPedidos, getItensPedido, postItensPedido, somaPedido } from "../models/itemPedido-models.js";
+import { putValorPedido } from "../models/pedidos-models.js";
 
 export const listarItensPedidos = async (req, res) => {
     try {
@@ -18,6 +19,29 @@ export const listarItensPedido = async (req, res) => {
     try {
         const ItensPedido = await getItensPedido(pedidoId)
         res.status(200).json(ItensPedido)
+    } catch (error) {
+        res.status(400).json({
+            "msg" : error.message,
+            "erro" : "true"
+        });
+    }
+}
+
+//Quando se cadastra um novo item em um pedido, é atualizado o valor total do pedido
+export const cadastrarItemPedido = async (req, res) => {
+    const {pedido_id, item_id, quantidade_itens} = req.body
+    const dataIP = new ItemPedido(pedido_id, item_id, quantidade_itens)
+
+    try {
+        const newItemPedido = await postItensPedido(dataIP) 
+        res.status(201).send(newItemPedido)
+        // totalPedido -> total dos itens do pedido especifico é atualizado quando cadastrado um novo pedido
+        let totalPedido = await somaPedido(pedido_id)
+        totalPedido = Object.values(totalPedido[0])
+        totalPedido = totalPedido[0]
+        
+        //atualizando o valor total do pedido na tabela de pedidos com o id do pedido específico
+        const updPedido = await putValorPedido(totalPedido, pedido_id)
     } catch (error) {
         res.status(400).json({
             "msg" : error.message,
