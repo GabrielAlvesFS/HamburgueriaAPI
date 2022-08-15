74e5885a1293b6f7eef986fa4a80c4db1bffe9e3
juliazib <julia.zibordi@gmail.com>
2022-08-15 15:14:12 -0300

    :sparkles: feat: todas as operações CRUD foram implementadas para a entidade item.

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,4 +1,5 @@
 import pedidos from "./pedidosRoutes.js";
+import itens from "./itemRoutes.js"
 
 const routes = (app) => {
     app.route('/').get((req, res) => {
@@ -6,7 +7,8 @@ const routes = (app) => {
     })
 
     app.use(
-        pedidos
+        pedidos,
+        itens
     );
 }
 
