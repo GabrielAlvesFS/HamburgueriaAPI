fabbd9cd0dc62408f9fd14381faa9a2a11a9ac4c
MagnusBittencourt <magnusbittencourt@gmail.com>
2022-08-16 00:32:12 -0300

    :sparkles: feat: Adicionando o controllers que Atualiza entregadores

--- src/controllers/entregadores-controllers.js
+++ src/controllers/entregadores-controllers.js
@@ -1,4 +1,4 @@
-import { Entregadores, getEntregadores, getEntregador, postEntregador } from "../models/entregadores-models.js";
+import { Entregadores, getEntregadores, getEntregador, postEntregador, putEntregador  } from "../models/entregadores-models.js";
 
 export const listarEntregadores = async (req, res) => {
     try {
@@ -31,10 +31,28 @@ export const criarEntregador = async (req, res) => {
     const dataE = new Entregadores(nome, cpf, telefone)
     try {
       const resposta = await postEntregador(dataE)
-      res.status(201).json(resposta)
+      res.status(200).json(resposta)
 
     } catch (error) {
       res.status(400).json({
+        "mensagem": error.message,
+        "erro": true
+      })
+    }
+  }
+
+  export const atualizarEntregador = async (req, res) => {
+    const {nome, cpf, telefone} = req.body
+    const dataE = new Entregadores(nome, cpf, telefone)
+    const id = req.params.id    
+    try {
+      const resposta = await putEntregador(dataE, id)
+      console.log(dataE)
+      console.log(id)
+      console.log(resposta)
+      res.status(200).json(resposta)
+    } catch (error) {
+      res.status(404).json({
         "mensagem": error.message,
         "erro": true
       })
