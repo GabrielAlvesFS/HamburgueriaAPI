c1f628458570a64e298920ed37461ff03c074ec4
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-04 21:21:24 -0300

    feat: initial structure

---
+++ src/controllers/authorization/postAuthorization.js
@@ -0,0 +1,17 @@
+// {
+//   email,
+//   senha -> enviada pelo user -> senha 
+//   role
+// }
+
+// export default async (req, res) => {
+//   try {
+//     const data = await 
+//     res.send(data)
+
+
+//   } catch (error) {
+//     logger.error(error)
+//     res.send(error)
+//   }
+// }
