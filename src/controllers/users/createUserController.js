156f92352ff365fb27ac5b43e44e5971fb3561f2
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-11 12:43:15 -0300

    feat: applying password encryption

--- src/controllers/users/createUserController.js
+++ src/controllers/users/createUserController.js
@@ -1,13 +1,19 @@
 import createUserValidator from "./validators/createUserValidator.js"
 import { createUser } from "../../services/users.js"
 import { logger } from "../../config/logger.js"
+import { hashValue } from "../../utils/bcrypt.js"
 
 export default async (req, res) => {
   try {
     // validação com o ZOD
     createUserValidator.parse(req.body)
+
+    // bcrypt
+    const user = req.body
+    user.password = await hashValue(user.password)
+
     // Fazendo o metodo POST
-    const data = await createUser(req.body)
+    const data = await createUser(user)
     res.send(data)
   } catch (error) {
     logger.error(error)
