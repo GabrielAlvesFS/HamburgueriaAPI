098531a2f5568c1730bcc7d5390f96cd93aa11b2
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-11 18:31:22 -0300

    :sparkles: feat: adding get user controller

---
+++ src/controllers/users/getUserController.js
@@ -0,0 +1,25 @@
+import { getUser } from "../../services/users.js";
+import { logger } from "../../config/logger.js";
+import getUserValidator from "./validators/getUserValidator.js"
+
+export default async (req, res) => {
+  try {
+    //Zod Validation
+    getUserValidator.parse(req.params.id)
+  
+    //GET with ID
+    const data = await getUser(req.params.id)
+
+    //Verifying if ID exists
+    if (!data) throw new Error("ID not found!")
+
+    //If ID exists, it will send the data
+    res.send(data)
+
+  } catch (error) {
+    if (error?.message === "ID not found!") return res.status(404).send({error: error.message})
+    else if (error.name === "ZodError") return res.status(400).send(error.issues)
+    logger.error(error)
+    res.status(500).send({ error: "internal error"})
+  }
+}
