89569ced60fff12b16afc42663e99ac27d459a69
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-11 12:41:12 -0300

    feat: bcrypt functions

--- src/utils/bcrypt.js
+++ src/utils/bcrypt.js
@@ -1,12 +1,12 @@
-// import bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
 
-// export const hashValue = async (value) => {
-//   const saltRounds = 13;
-//   const hashValue = await bcrypt.hash(value, saltRounds);
-//   return hashValue;
-// };
+export const hashValue = async (value) => {
+  const saltRounds = 13;
+  const hashValue = await bcrypt.hash(value, saltRounds);
+  return hashValue;
+};
 
-// export const compareHash = async (value, hash) => {
-//   const result = await bcrypt.compare(value, hash);
-//   return result;
-// };
+export const compareHash = async (value, hash) => {
+  const result = await bcrypt.compare(value, hash);
+  return result;
+};
