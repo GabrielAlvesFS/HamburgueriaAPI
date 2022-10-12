1faf53aa0dd3f10bc82ba349d2f2df3c693bdef9
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-12 14:28:10 -0300

    :sparkles: feat: adding get into router

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import postUser from "../controllers/users/postUserController.js";
 import listUsers from "../controllers/users/listUsersController.js";
+import getUser from "../controllers/users/getUserController.js";
+
 
 const router = express.Router();
 
 router
     .post("/v1/user", postUser)
     .get("/v1/users", listUsers)
+    .get("/v1/user/:id", getUser)
     
 export default router;
