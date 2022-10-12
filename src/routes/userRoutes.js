561ccb523f9c9ea0fdb8034811241fdacff151be
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-12 16:04:15 -0300

    :sparkles: feat: adding the patch method into routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import postUser from "../controllers/users/postUserController.js";
 import listUsers from "../controllers/users/listUsersController.js";
 import getUser from "../controllers/users/getUserController.js";
-
+import patchUser from "../controllers/users/patchUserController.js";
 
 const router = express.Router();
 
@@ -10,5 +10,6 @@ router
     .post("/v1/user", postUser)
     .get("/v1/users", listUsers)
     .get("/v1/user/:id", getUser)
+    .patch("/v1/user/:id", patchUser)
     
 export default router;
