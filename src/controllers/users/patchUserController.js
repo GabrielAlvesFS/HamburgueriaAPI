799524094cedd85f77cecd665da0164338e8308f
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-17 17:54:19 -0300

    :boom: fix: removing unnecessary objects

--- src/controllers/users/patchUserController.js
+++ src/controllers/users/patchUserController.js
@@ -2,37 +2,20 @@ import { updateUser } from "../../services/users.js";
 import { getUser } from "../../services/users.js";
 import { logger } from "../../config/logger.js";
 import patchUserValidator from "./validators/patchUserValidator.js"
-import { hashValue } from "../../utils/bcrypt.js"
 
 export default async (req, res) => {
   try {
-    const dataObj = { 
-      id: req.params.id,
-      name: req.body?.name,
-      password: req.body?.password,
-      birthDate: req.body?.birthDate,
-      phone: req.body?.phone
-    }
-  
     //Zod Validation
-    patchUserValidator.parse(dataObj)
+    patchUserValidator.parse({...req.params, ...req.body})
     
     //Verifying if ID exists
-    const user = await getUser(dataObj.id)
+    const user = await getUser(req.params.id)
     if (!user) throw new Error("ID not found!")
 
-    const dataAfterValidated = {
-      name: dataObj?.name,
-      password: dataObj?.password,
-      birthDate: dataObj?.birthDate,
-      phone: dataObj?.phone
-    }
-
-    //If it exists the password field will be encrypted
-    dataAfterValidated.password = await hashValue(dataAfterValidated.password)
-
     //Updating User
-    const data = await updateUser(dataObj.id, dataAfterValidated)
+    if (req.body.id) delete req.body.id
+
+    const data = await updateUser(req.params.id, req.body)
     res.send(data)
   } catch (error) {
     if (error?.message === "ID not found!") return res.status(404).send({error: error.message})
