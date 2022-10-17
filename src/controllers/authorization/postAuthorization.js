d02accd63419938f44519534e294b7d4f2eef1c6
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-17 19:13:57 -0300

    :sparkles: feat: POST authorization controller

--- src/controllers/authorization/postAuthorization.js
+++ src/controllers/authorization/postAuthorization.js
@@ -1,22 +1,48 @@
-import { findUser } from "../../services/users.js";
-import { compareHash } from "../../utils/bcrypt.js"
+import jwt from "jsonwebtoken";
+import { listUsers } from "../../services/users.js";
+import { compareHash } from "../../utils/bcrypt.js";
+import { logger } from "../../config/logger.js";
 
 export default async (req, res) => {
   try {
-    //coisas que imagino
-    if(req.body.role == 'customer') {
-      const user = await findUser({email: req.body.email})
-    } 
+    let user;
 
-    if(req.body.role == 'admin') {
-      const user = await something// Aqui faz o get da tabela de admins
+    if(req.body.role == 'customer') {
+      user = await listUsers({email: req.body.email})
     }
 
-    const data = await compareHash(req.body.password, user.password)
-    res.send(data)
+    else if(req.body.role == 'manager') {
+      user = await something// Aqui faz o get da tabela de admins
+    }
+
+    if (!user.length) throw new Error('User not found!')
+    
+    const checkPassword = await compareHash(req.body.password, user[0].password)
+
+    if (!checkPassword) throw new Error('Wrong password or email!')
+    
+    const payload = {
+      nome: user[0].name,  
+      email: user[0].email,
+      exp: Math.floor(Date.now() / 1000) + (7*24*60*60)
+    }
+
+    const secret = process.env.JWT_SECRET
+
+    const token = jwt.sign(payload, secret)
+
+    // retorna  obj token e payload
+    res.status(200).send({
+      status: "Autenticação funciona!",
+      token, 
+      payload
+    })
     
   } catch (error) {
+    if (error.message === 'User not found!') return res.status(404).send({error: error.message})
+    if (error.message === 'Wrong password or email!') return res.status(403).send({error: error.message})
+
     logger.error(error)
-    res.send(error)
+    res.status(500).send({ error: "internal error"})
   }
 }
