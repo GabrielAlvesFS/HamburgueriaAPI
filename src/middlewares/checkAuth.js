8b2813d1c178a6f78e0c78c019d7220a079d06b3
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-19 03:28:17 -0300

    :sparkles: feat: middleware to check authentication

--- src/middlewares/checkAuth.js
+++ src/middlewares/checkAuth.js
@@ -1,24 +1,24 @@
-// import jwt from "jsonwebtoken";
-// import { logger } from "../config/logger.js";
+import jwt from "jsonwebtoken";
+import { logger } from "../config/logger.js";
 
-// export default (...types) =>
-//   async (req, res, next) => {
-//     try {
-//       const [, token] = req.headers.authorization.split(" ");
-//       const jwtSecret = process.env.JWT_SECRET;
-//       const decodedToken = jwt.verify(token, jwtSecret);
-//       if (
-//         !types.find(
-//           (type) =>
-//             decodedToken.roles.some((role) => role === type) ||
-//             decodedToken.roles.includes("admin")
-//         )
-//       )
-//         throw new Error("invalid token for this requisiton");
-//       req.payload = decodedToken;
-//       next();
-//     } catch (e) {
-//       logger.error(e);
-//       res.status(401).send({message: "unathorized"})
-//     }
-//   };
+export default (...types) =>
+  async (req, res, next) => {
+    try {
+      const [, token] = req.headers.authorization.split(" ");
+      const jwtSecret = process.env.JWT_SECRET;
+      const decodedToken = jwt.verify(token, jwtSecret);
+      if (
+        !types.find(
+          (type) =>
+            decodedToken.roles.some((role) => role === type) ||
+            decodedToken.roles.includes("manager")
+        )
+      )
+        throw new Error("invalid token for this requisiton");
+      req.payload = decodedToken;
+      next();
+    } catch (e) {
+      logger.error(e);
+      res.status(401).send({message: "unathorized"})
+    }
+  };
