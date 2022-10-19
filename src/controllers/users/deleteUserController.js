4aed3ee72ef5626470ee483ebd66a3fbaf15fa24
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-19 03:23:13 -0300

    :sparkles: feat: adding delete user controller

---
+++ src/controllers/users/deleteUserController.js
@@ -0,0 +1,27 @@
+import { getUser } from "../../services/users.js";
+import { deleteUser } from "../../services/users.js";
+import { logger } from "../../config/logger.js";
+import deleteUserValidator from "./validators/deleteUserValidator.js";
+
+export default async (req, res) => {
+  try {
+    //Zod Validation
+    deleteUserValidator.parse(req.params.id)
+
+    //Veryfing if user exists
+    const user = await getUser(req.params.id)
+    if (!user) throw new Error("ID not found!")
+    
+    //If ID exists, the user will be deleted
+    const data = await deleteUser({
+      _id: req.params.id
+    })
+    res.send(data)
+
+  } catch (error) {
+    if (error?.message === "ID not found!") return res.status(404).send({error: error.message})
+    else if (error.name === "ZodError") return res.status(400).send(error.issues)
+    logger.error(error)
+    res.status(500).send({ error: "internal error"})
+  }
+}
