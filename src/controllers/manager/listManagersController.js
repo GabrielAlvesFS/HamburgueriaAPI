fb95400b84b2f90eab21cab5e3c8a50f65e54c2f
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-20 19:45:51 -0300

    :sparkles: feat: Verifying if manager exists

--- src/controllers/manager/listManagersController.js
+++ src/controllers/manager/listManagersController.js
@@ -1,19 +1,20 @@
 import listManagersValidator from "./validators/listManagersValidator.js";
 import { listManagers } from "../../services/manager.js";
-import { logger } from "../../config/logger.js"
+import { logger } from "../../config/logger.js";
 
 export default async (req, res) => {
   try {
     // Validation with ZOD
     listManagersValidator.parse(req.body)
 
-    // Verifying if user exists
+    // Verifying if manager exists
+    const manager = await listManagers(req.body)
+    if (!manager.length) throw new Error("Manager not found!")
 
-
-    const data = await listManagers(req.body)
-    res.status(200).send(data)
+    res.status(200).send(manager)
 
   } catch (error) {
+    if (error?.message === "Manager not found!") return res.status(404).send({error: error.message})
     if (error.name === "ZodError") return res.status(400).send(error.issues)
     logger.error(error)
     res.status(500).send({ error: "internal error"})
