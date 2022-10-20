cc97ea73e5c10ac2bd1b9276e1ff373b8b975755
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-20 19:33:35 -0300

    :sparkles: feat: adding the get method into routes

--- src/routes/managerRoutes.js
+++ src/routes/managerRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import postManager from "../controllers/manager/postManagerController.js";
 import listManagers from "../controllers/manager/listManagersController.js";
+import getManager from "../controllers/manager/getManagerController.js";
 
 const router = express.Router();
 
 router
     .post("/v1/manager", postManager)
     .get("/v1/manager", listManagers)
+    .get("/v1/manager/:id", getManager)
     
 export default router;
