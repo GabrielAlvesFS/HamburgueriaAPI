32feb84eaef5734264fffd1a1d8b6b7d58184ca0
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-21 15:53:06 -0300

    :sparkles: feat: validation of patch method

---
+++ src/controllers/manager/validators/patchManagerValidator.js
@@ -0,0 +1,6 @@
+import zod  from 'zod';
+
+export default zod.object({
+  id: zod.string().refine((value) => /^[a-f\d]{24}$/.test(value), {message: "Invalid ID!"}),
+  name: zod.string().min(3).max(100).optional()
+}).strict()
