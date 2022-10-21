4a18f33fea5d28f1348684f39abaa500e2e4d602
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-21 15:56:48 -0300

    :sparkles: feat: adding the patch and delete method into routes

--- src/routes/managerRoutes.js
+++ src/routes/managerRoutes.js
@@ -2,6 +2,8 @@ import express from "express";
 import postManager from "../controllers/manager/postManagerController.js";
 import listManagers from "../controllers/manager/listManagersController.js";
 import getManager from "../controllers/manager/getManagerController.js";
+import patchManager from "../controllers/manager/patchManagerController.js";
+import deleteManager from "../controllers/manager/deleteManagerController.js";
 
 const router = express.Router();
 
@@ -9,5 +11,7 @@ router
     .post("/v1/manager", postManager)
     .get("/v1/manager", listManagers)
     .get("/v1/manager/:id", getManager)
+    .patch("/v1/manager/:id", patchManager)
+    .delete("/v1/manager/:id", deleteManager)
     
 export default router;
