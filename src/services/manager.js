0ff75cf09b225339b2d20813d8a66cfbfb89cc00
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-21 15:55:55 -0300

    :sparkles: feat: adding the patch and delete method

--- src/services/manager.js
+++ src/services/manager.js
@@ -13,3 +13,13 @@ export const listManagers = async (query) => {
 export const getManager = async (id) => {
   return await manager.findById(id);
 }
+
+// UPDATE
+export const updateManager = async (id, data) => {
+  return await manager.updateOne({_id: id}, { $set: {...data}})
+}
+
+// DELETE
+export const deleteManager = async (id) => {
+  return await manager.deleteOne(id)
+}
