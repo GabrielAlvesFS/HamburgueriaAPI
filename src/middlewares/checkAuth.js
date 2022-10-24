0b7bbc38ab5fd3a1dd55143b2342cf74789ebc1f
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-24 19:11:09 -0300

    :bug: fix: fixing the role verification

--- src/middlewares/checkAuth.js
+++ src/middlewares/checkAuth.js
@@ -4,21 +4,23 @@ import { logger } from "../config/logger.js";
 export default (...types) =>
   async (req, res, next) => {
     try {
+      if (!req.headers.authorization) throw new Error("No token provided")
       const [, token] = req.headers.authorization.split(" ");
       const jwtSecret = process.env.JWT_SECRET;
       const decodedToken = jwt.verify(token, jwtSecret);
       if (
         !types.find(
           (type) =>
-            decodedToken.roles.some((role) => role === type) ||
-            decodedToken.roles.includes("manager")
+            decodedToken.roles.split().some((role) => role === type)
         )
       )
         throw new Error("invalid token for this requisiton");
       req.payload = decodedToken;
       next();
-    } catch (e) {
-      logger.error(e);
+    } catch (error) {
+      if (error.message === "No token provided") return res.status(401).send({ error: error.message})
+      else if (error.message === "invalid token for this requisiton") return res.status(401).send({ error: error.message})
+      logger.error(error);
       res.status(401).send({message: "unathorized"})
     }
   };
