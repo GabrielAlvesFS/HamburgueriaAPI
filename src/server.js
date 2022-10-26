6db8cb6415bcef01f5ad1ba6bdc34aa33bf3fb99
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-26 20:17:24 -0300

    :sparkles: feat: function to peform all migrations

--- src/server.js
+++ src/server.js
@@ -1,7 +1,18 @@
+import { logger } from "./config/logger.js";
 import app from "./app.js";
 import connectionMongo from './config/mongo/connection.js';
+import { database, up } from 'migrate-mongo';
 
 connectionMongo()
+
+const runMigrationsMongo = async () => {
+    const { db, client } = await database.connect();
+    const migrations = await up(db, client);
+    const migrationsNames = migrations.toString().split(",")
+    migrationsNames.forEach( migrationName => logger.info(`Migration ${migrationName} successfully executed!`))
+}
+
+runMigrationsMongo()
 
 const port = process.env.PORT || 3000;
 
