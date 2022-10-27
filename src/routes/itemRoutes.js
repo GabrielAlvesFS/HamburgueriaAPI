c5b3c04ed3c79de21f89148bff9f27a590330a5e
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-27 16:35:21 -0300

    :sparkles: feat: adding the get and patch method into routes

--- src/routes/itemRoutes.js
+++ src/routes/itemRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import postItem from "../controllers/items/postItemController.js"
+import postItem from "../controllers/items/postItemController.js";
 import listItems from "../controllers/items/listItemsController.js";
+import getItem from "../controllers/items/getItemController.js";
+import patchItem from "../controllers/items/patchItemController.js";
+
 const router = express.Router();
 
 router
     .post("/v1/item", postItem)
     .get("/v1/item", listItems)
+    .get("/v1/item/:id", getItem)
+    .patch("/v1/item/:id", patchItem)
     
 export default router;  
