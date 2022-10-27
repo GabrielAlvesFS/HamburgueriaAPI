8336c39d743305e18e1f0bc43d58a9d34ace67a9
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-27 15:34:07 -0300

    :sparkles: feat: validation of get method

---
+++ src/controllers/items/validators/listItemsValidator.js
@@ -0,0 +1,7 @@
+import zod from 'zod';
+
+export default zod.object({
+  type: zod.string().min(3).max(14).optional(),
+  name: zod.string().min(3).max(40).optional(),
+  value: zod.string().optional(),
+}).strict()
