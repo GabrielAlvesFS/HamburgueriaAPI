1db7f6c958cfff87745907a8e497b74631890485
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-27 17:25:39 -0300

    :sparkles: feat: adding the delete method into routes

--- src/routes/itemRoutes.js
+++ src/routes/itemRoutes.js
@@ -3,6 +3,7 @@ import postItem from "../controllers/items/postItemController.js";
 import listItems from "../controllers/items/listItemsController.js";
 import getItem from "../controllers/items/getItemController.js";
 import patchItem from "../controllers/items/patchItemController.js";
+import deleteItem from "../controllers/items/deleteItemController.js";
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router
     .get("/v1/item", listItems)
     .get("/v1/item/:id", getItem)
     .patch("/v1/item/:id", patchItem)
+    .delete("/v1/item/:id", deleteItem)
     
 export default router;  
