416206bb30ec12ffa5787988049fe96621a9953a
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-10-27 16:28:42 -0300

    :sparkles: feat: validation of patch method

---
+++ src/controllers/items/validators/patchItemValidator.js
@@ -0,0 +1,10 @@
+import zod from 'zod';
+
+export default zod.object({
+  id: zod.string().refine((value) => /^[a-f\d]{24}$/.test(value), {message: "Invalid ID!"}),
+  type: zod.string().min(3).max(14).optional(),
+  name: zod.string().min(3).max(40).optional(),
+  description: zod.string().min(3).max(100).optional(),
+  value: zod.string().optional(),
+  imgUrl: zod.string().max(2048).optional()
+}).strict()
