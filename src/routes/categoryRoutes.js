38da1df5e312ec4ec6279dec8cf9515e8de1141b
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-11-03 16:07:01 -0300

    :sparkles: feat: adding get method into routes

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import postCategory from '../controllers/category/postCategoryController.js';
+import listCategories from '../controllers/category/listCategoriesController.js';
+import getCategory from '../controllers/category/getCategoryController.js';
 
 const router = express.Router()
 
 router
     .post("/v1/category", postCategory)
+    .get("/v1/category", listCategories)
+    .get("/v1/category/:id", getCategory)
+
 
 export default router;   
