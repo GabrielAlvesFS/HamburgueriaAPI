b30bca2bd175b1c1cb457ceb88660b0a83ccc580
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-11-03 16:22:26 -0300

    :sparkles: feat: adding patch method into routes

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import postCategory from '../controllers/category/postCategoryController.js';
 import listCategories from '../controllers/category/listCategoriesController.js';
 import getCategory from '../controllers/category/getCategoryController.js';
+import patchCategory from '../controllers/category/patchCategoryController.js';
 
 const router = express.Router()
 
@@ -9,6 +10,6 @@ router
     .post("/v1/category", postCategory)
     .get("/v1/category", listCategories)
     .get("/v1/category/:id", getCategory)
-
+    .patch("/v1/category/:id", patchCategory)
 
 export default router;   
