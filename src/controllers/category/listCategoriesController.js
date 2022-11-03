dcdc884ccc2f734837ead2da1a884a0a506517af
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-11-03 16:07:50 -0300

    :sparkles: feat: adding get categories controller to list categories

---
+++ src/controllers/category/listCategoriesController.js
@@ -0,0 +1,22 @@
+import listCategoriesValidator from "./validations/listCategoriesValidator.js";
+import { listCategories } from "../../services/category.js";
+import { logger } from "../../config/logger.js";
+
+export default async (req, res) => {
+  try {
+    // Validation with ZOD
+    listCategoriesValidator.parse(req.body)
+
+    // Verifying if category exists
+    const data = await listCategories(req.body);
+    if (!data[0]) throw new Error("Category not found!")
+
+    res.status(200).send(data)
+
+  } catch (error) {
+    if (error.message === "Category not found!") return res.status(404).send({error: error.message})
+    else if (error.name === "ZodError") return res.status(400).send({error: error.issues})
+    logger.error(error)
+    res.status(500).send({error: "Internal error"})
+  }
+}
