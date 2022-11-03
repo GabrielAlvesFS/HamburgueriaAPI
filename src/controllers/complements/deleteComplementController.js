d833fe7ca5a1c58e7302556326fc63efd7a82099
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-11-03 15:08:14 -0300

    :sparkles: feat: adding delete complement controller

---
+++ src/controllers/complements/deleteComplementController.js
@@ -0,0 +1,25 @@
+import deleteComplementValidator from "./validations/deleteComplementValidator.js";
+import { getComplement, deleteComplement } from "../../services/complement.js";
+import { logger } from "../../config/logger.js";
+
+export default async (req, res) => {
+  try {
+    // Validation with ZOD
+    deleteComplementValidator.parse(req.params.id)
+
+    // Veryfing if complement exists
+    const complement = await getComplement(req.params.id)
+    if (!complement) throw new Error("Complement not found!")
+
+    // If complement exists it will be deleted
+    const data = await deleteComplement({
+      _id: req.params.id
+    })
+    res.status(200).send(data)
+
+  } catch (error) {
+    if (error.name === "ZodError") return res.status(400).send({error: error.issues})
+    logger.error(error)
+    res.status(500).send({error: "Internal error"})
+  }
+}
