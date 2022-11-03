37ceb47d2c39f7dc2dc0b4ebbd07db6d67331eb1
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-11-03 14:35:08 -0300

    :sparkles: feat: adding get method into routes

--- src/routes/complementRoutes.js
+++ src/routes/complementRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import postComplement from '../controllers/complements/postComplementController.js';
 import listComplements from '../controllers/complements/listComplementsController.js';
+import getComplement from '../controllers/complements/getComplementController.js';
 
 const router = express.Router();
 
 router
     .post("/v1/complement", postComplement)
     .get("/v1/complement", listComplements)
-    
+    .get("/v1/complement/:id", getComplement)
+
 export default router;   
