e9fb85ac9d909e3be4d53e9e08a6f3c18253c39d
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-11-03 16:05:25 -0300

    :sparkles: feat: adding get method

--- src/services/category.js
+++ src/services/category.js
@@ -4,3 +4,12 @@ import categoryModel from "../models/categoryModel.js";
 export const postCategory = async (data) => {
   return await categoryModel.create(data);
 }
+
+// GET
+export const listCategories = async (query) => {
+  return await categoryModel.find(query);
+}
+
+export const getCategory = async (id) => {
+  return await categoryModel.findById(id);
+}
