0fd36ca5edb37be7d27b7715dfb459835bbe30d5
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-11-03 14:56:30 -0300

    :sparkles: feat: adding patch method into routes

--- src/routes/complementRoutes.js
+++ src/routes/complementRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import postComplement from '../controllers/complements/postComplementController.js';
 import listComplements from '../controllers/complements/listComplementsController.js';
 import getComplement from '../controllers/complements/getComplementController.js';
+import patchComplement from '../controllers/complements/patchComplementController.js';
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ router
     .post("/v1/complement", postComplement)
     .get("/v1/complement", listComplements)
     .get("/v1/complement/:id", getComplement)
+    .patch("/v1/complement/:id", patchComplement)
 
 export default router;   
