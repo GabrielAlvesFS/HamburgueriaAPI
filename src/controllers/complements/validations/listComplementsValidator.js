4dbbd48dd2f94a72fa68719866ec7362379ec313
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-11-17 11:10:30 -0300

    :bug: fix: fixing typo

--- src/controllers/complements/validations/listComplementsValidator.js
+++ src/controllers/complements/validations/listComplementsValidator.js
@@ -1,7 +1,7 @@
 import zod from 'zod';
 
 export default zod.object({
-  tilte: zod.string().min(3).max(100).optional(),
+  title: zod.string().min(3).max(100).optional(),
   required: zod.enum(["true", "false"]).optional(),
   min: zod.string().optional(),
   max: zod.string().optional()
