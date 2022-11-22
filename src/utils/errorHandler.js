44af138fc7fdd20f0dd92f07491639a27de92381
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-11-22 19:57:06 -0300

    :sparkles: feat: making custom errors

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,30 +1,70 @@
 import { logger } from "../config/logger.js"
 
+export class NotFoundError extends Error {
+  constructor(message, path) {
+    super(message)
+    this.name = "notFoundError"
+    this.code = "notFoundError"
+    this.path = path
+  }
+}
+
+export class AssignmentError extends Error {
+  constructor(message, path) {
+    super(message)
+    this.name = "assignmentError"
+    this.code = "assignmentError"
+    this.path = path
+  }
+}
+
+const notFoundError = (err, res) => {
+  return res.status(404).send(
+    {
+      error: [{
+        code: err.code,
+        message: err.message,
+        path: [ err.path ]
+      }]
+  })
+}
+
+const assignmentError = (err, res) => {
+  return res.status(400).send(
+    {
+      error: [{
+        code: err.code,
+        message: err.message,
+        path: [ err.path ]
+      }]
+  })
+}
+
 const zodError = (err, res) => {
   return res.status(400).send({error: err.issues})
 }
 
-const notFound = (err, res) => {
-  return res.status(404).send({error: err.message})
-}
-
 const errorFunctions = { 
-  notFound,
+  notFoundError,
+  assignmentError,
   ZodError: zodError
 }
 
-export default async (err, req, res, next) => {
+export const errorHandler = async (err, req, res, next) => {
   try {
     
-    const errorName = err.message || err.name
-    errorFunctions[errorName](err, res)
-    res.status(500).send({error: "Internal error"})
+    const errorName = err.name || err.message
+    return errorFunctions[errorName](err, res)
     
-    res.send(err)
-    throw err
-
   } catch (error) {
-
+    res.status(500).send({
+      error: [{
+        code: "internalServerError",
+        message: "Internal Server Error",
+        path: []
+      }]
+    })
+    
     logger.error(error)
     next()
 
