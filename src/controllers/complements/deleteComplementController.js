4ed7ddbaff56bcb962cd3bcadd6bb19cdbcbbc11
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-11-22 19:54:20 -0300

    :recycle: refactor: applying validations and throwing erros to error handler

--- src/controllers/complements/deleteComplementController.js
+++ src/controllers/complements/deleteComplementController.js
@@ -1,25 +1,20 @@
-import deleteComplementValidator from "./validations/deleteComplementValidator.js";
-import { getComplement, deleteComplement } from "../../services/complement.js";
-import { logger } from "../../config/logger.js";
+import { validate } from "./validations/deleteComplementValidator.js";
+import { deleteComplement } from "../../services/complement.js";
 
-export default async (req, res) => {
+export default async (req, res, next) => {
   try {
-    // Validation with ZOD
-    deleteComplementValidator.parse(req.params.id)
+    // Validations
+    await validate(req.params)
 
-    // Verifying if complement exists
-    const complement = await getComplement(req.params.id)
-    if (!complement) throw new Error("Complement not found!")
-
-    // If complement exists it will be deleted
+    // If the complement passes the validations it will be deleted
     const data = await deleteComplement({
       _id: req.params.id
     })
     res.status(200).send(data)
 
   } catch (error) {
-    if (error.name === "ZodError") return res.status(400).send({error: error.issues})
-    logger.error(error)
-    res.status(500).send({error: "Internal error"})
+    // Throwing to error handler
+    next(error)
+    
   }
 }
--- src/controllers/complements/patchComplementController.js
+++ src/controllers/complements/patchComplementController.js
@@ -1,15 +1,10 @@
-import patchComplementValidator from './validations/patchComplementValidator.js'
-import { getComplement, updateComplement } from "../../services/complement.js";
-import { logger } from "../../config/logger.js";
+import { validate } from './validations/patchComplementValidator.js';
+import { updateComplement } from "../../services/complement.js";
 
-export default async (req, res) => {
+export default async (req, res, next) => {
   try {
-    // Validation with ZOD
-    patchComplementValidator.parse({...req.params, ...req.body})
-
-    // Verifying if complement exists
-    const complement = await getComplement(req.params.id)
-    if (!complement) throw new Error("Complement not found!")
+    // Validations
+    await validate(req.params, req.body)
 
     //Removing id from body if exists
     if (req.body.id) delete req.body.id
@@ -18,9 +13,8 @@ export default async (req, res) => {
     res.status(200).send(data)
 
   } catch (error) {
-    if (error?.message === "Complement not found!") return res.status(404).send({error: error.message})
-    else if (error.name === "ZodError") return res.status(400).send({error: error.issues})
-    logger.error(error)
-    res.status(500).send({error: "Internal error"})
+    // Throwing to error handler
+    next(error)
+    
   }
 }
