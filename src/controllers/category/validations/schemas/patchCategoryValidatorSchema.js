6a7851f67250e3345deb72dfc39a5a11a81e3aef
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-12-01 15:05:23 -0300

    ✨ feat: validations for schemas

---
+++ src/controllers/category/validations/schemas/patchCategoryValidatorSchema.js
@@ -0,0 +1,8 @@
+import zod from 'zod';
+import { isValidObjectId } from '../../../../utils/validations.js';
+
+export default zod.object({
+  id: zod.string().refine( isValidObjectId, {message: "Invalid ID!"} ),
+  name: zod.string().min(3).max(100).optional(),
+  imgUrl: zod.string().max(2048).optional()
+}).strict()
