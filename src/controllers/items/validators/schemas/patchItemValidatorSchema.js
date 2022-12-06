2a63318802ce6f62c893095d7d3fc7946996a583
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-12-06 15:58:29 -0300

    ✨ feat: validations for schemas

---
+++ src/controllers/items/validators/schemas/patchItemValidatorSchema.js
@@ -0,0 +1,13 @@
+import zod from 'zod';
+import { isValidObjectId } from '../../../../utils/validations.js';
+
+export default zod.object({
+  id: zod.string().refine( isValidObjectId, {message: "Invalid ID!"}),
+  type: zod.string().min(3).max(14).optional(),
+  active: zod.boolean().optional(),
+  name: zod.string().min(3).max(40).optional(),
+  description: zod.string().min(3).max(100).optional(),
+  value: zod.string().optional(),
+  imgUrl: zod.string().max(2048).optional(),
+  complementsIds: zod.array().optional()
+}).strict()
---
+++ src/controllers/users/validators/schemas/patchUserValidatorSchema.js
@@ -0,0 +1,9 @@
+import zod  from 'zod';
+import { isValidObjectId } from '../../../../utils/validations.js';
+
+export default zod.object({
+  id: zod.string().refine( isValidObjectId, {message: "Invalid ID!"} ),
+  name: zod.string().min(3).max(100).optional(),
+  birthDate: zod.preprocess((dateString) => new Date(zod.string(dateString).min(10, { message: "The date string must be at least 10 characters long"}).parse(dateString)), zod.date()).optional(),
+  phone: zod.string().optional(),
+}).strict()
