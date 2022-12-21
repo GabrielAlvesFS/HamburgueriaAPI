58560925a91d2ac849965b48c18688f6908dccdb
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-12-21 11:18:46 -0300

    :sparkles: feat: adding active field

--- src/models/categoryModel.js
+++ src/models/categoryModel.js
@@ -3,7 +3,8 @@ const { Schema } = mongoose;
 
 const categorySchema = new Schema({
   name: {type: String, required: true},
-  imgUrl: {type: String}
+  imgUrl: {type: String}, 
+  active: {type: Boolean, required: true}
 }, {timestamps: true})
 
 const category = mongoose.model('Category', categorySchema);
