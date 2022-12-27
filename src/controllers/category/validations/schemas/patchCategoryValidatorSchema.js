ea6ef50a2f91024efdab9082f49c82c82c41bbc0
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2022-12-27 12:07:17 -0300

    :sparkles: feat: Validation in new field of schema

--- src/controllers/category/validations/schemas/patchCategoryValidatorSchema.js
+++ src/controllers/category/validations/schemas/patchCategoryValidatorSchema.js
@@ -4,5 +4,6 @@ import { isValidObjectId } from '../../../../utils/validations.js';
 export default zod.object({
   id: zod.string().refine( isValidObjectId, {message: "Invalid ID!"} ),
   name: zod.string().min(3).max(100).optional(),
-  imgUrl: zod.string().max(2048).optional()
+  imgUrl: zod.string().max(2048).optional(),
+  active: zod.boolean().optional()
 }).strict()
