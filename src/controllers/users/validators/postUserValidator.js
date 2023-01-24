7096184f3eca148dc0e62f8e1b741779fd2cd553
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-01-24 17:44:21 -0300

    :boom: fix: fixing phone validation

--- src/controllers/users/validators/postUserValidator.js
+++ src/controllers/users/validators/postUserValidator.js
@@ -7,5 +7,5 @@ export default zod.object({
   cpf: zod.string().length(11).refine(isValidCPF, (value) => ({message: `invalid CPF: ${value}`}) ),
   password: zod.string(),
   birthDate: zod.preprocess((dateString) => new Date(zod.string(dateString).min(10, { message: "The date string must be at least 10 characters long"}).parse(dateString)), zod.date()),
-  phone: zod.string().optional()
+  phone: zod.string().min(11).max(11)
 }).strict()
