b7dba8fd4df6a25c653525b0a48733c64bb06753
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-01-25 11:22:58 -0300

    ✨ feat: adding get method into routes

--- src/routes/addressRoutes.js
+++ src/routes/addressRoutes.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import checkAuth from "../middlewares/checkAuth.js";
 import postAddress from '../controllers/addresses/postAddressController.js';
+import listAddresses from '../controllers/addresses/listAddressesController.js';
+import getAddress from '../controllers/addresses/getAddressController.js';
 
 const router = express.Router()
 
 router
     .post("/v1/address", checkAuth("customer"), postAddress)
+    .get("/v1/address", checkAuth("customer"), listAddresses)
+    .get("/v1/address/:id", checkAuth("customer"), getAddress)
 
-export default router;  
+export default router;
