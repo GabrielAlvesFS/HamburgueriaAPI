23ac44287d9cb7f96ae5e0638783f20896de1de4
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-01-25 11:24:02 -0300

    ✨ feat: adding get method

--- src/services/address.js
+++ src/services/address.js
@@ -4,3 +4,12 @@ import addressModel from "../models/addressModel.js";
 export const postAddress = async (data) => {
   return await addressModel.create(data);
 }
+
+// GET
+export const getAddress = async (id) => {
+  return await addressModel.findById(id)
+}
+
+export const listAddresses = async (query) => {
+  return await addressModel.find(query);
+}
