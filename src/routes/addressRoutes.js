8e8f35fcac19c91399f3878ffd17e19018125390
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-01-26 10:20:21 -0300

    ✨ feat: adding patch method into routes

--- src/routes/addressRoutes.js
+++ src/routes/addressRoutes.js
@@ -3,6 +3,7 @@ import checkAuth from "../middlewares/checkAuth.js";
 import postAddress from '../controllers/addresses/postAddressController.js';
 import listAddresses from '../controllers/addresses/listAddressesController.js';
 import getAddress from '../controllers/addresses/getAddressController.js';
+import patchAddress from '../controllers/addresses/patchAddressController.js';
 
 const router = express.Router()
 
@@ -10,5 +11,6 @@ router
     .post("/v1/address", checkAuth("customer"), postAddress)
     .get("/v1/address", checkAuth("customer"), listAddresses)
     .get("/v1/address/:id", checkAuth("customer"), getAddress)
+    .patch("/v1/address/:id", checkAuth("customer"), patchAddress)
 
 export default router;
