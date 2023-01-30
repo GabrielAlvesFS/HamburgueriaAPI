749f6ffa8f45453526572d1b1133327d6164ffa1
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-01-30 09:49:53 -0300

    :sparkles: feat: adding new validations

--- src/controllers/items/validators/patchItemValidator.js
+++ src/controllers/items/validators/patchItemValidator.js
@@ -1,5 +1,7 @@
 import patchItemValidatorSchema from "./schemas/patchItemValidatorSchema.js";
 import { getItem } from "../../../services/items.js";
+import { getCategory } from "../../../services/category.js";
+import { getComplement } from "../../../services/complement.js";
 import { NotFoundError } from "../../../utils/errorHandler.js";
 
 export const validate = async (params, body) => {
@@ -10,6 +12,20 @@ export const validate = async (params, body) => {
     // Verifying if Item exists
     const item = await getItem(params.id)
     if (!item) throw new NotFoundError("This item doesn't exist!", "id")
+
+    if (body?.categoryId) { 
+      const category = await getCategory(body.categoryId)
+      if (!category) throw new NotFoundError("This category doesn't exist!", {categoryId: body.categoryId})
+    }
+
+    if (body?.complementsIds) {
+      await Promise.all(
+        body.complementsIds.map( async (id) => {
+          const complement = await getComplement(id);
+          if (!complement) throw new NotFoundError("This complement group doesn't exist!", {complementsIds: [id]})
+        })
+      )
+    }
 
   } catch (error) {
     // Throwing error to controller
