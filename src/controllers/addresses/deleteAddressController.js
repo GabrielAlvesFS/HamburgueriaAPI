2a46db91389fc4224a4a470b5b98e23ffcbc717c
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-01-31 11:37:45 -0300

    :sparkles: feat: updating the user's addressIds field when deleting the address

--- src/controllers/addresses/deleteAddressController.js
+++ src/controllers/addresses/deleteAddressController.js
@@ -1,13 +1,25 @@
 import { deleteAddress } from "../../services/address.js";
-import deleteAddressValidator from "./validations/deleteAddressValidator.js";
+import { validate } from "./validations/deleteAddressValidator.js";
+import { getUser, updateUser } from "../../services/users.js";
 
 export default async (req, res, next) => {
   try {
-    deleteAddressValidator.parse(req.params.id)
+    await validate(req.params)
 
     const data = await deleteAddress({
       _id: req.params.id
     })
+
+    const user = await getUser(req.payload.id, "-password")
+    
+    const address = user.addressIds.filter((address) => {
+      if (address !== req.params.id) {
+        return address
+      }
+    })
+
+    const newAddress = await updateUser(req.payload.id, {addressIds: address})
+
     res.status(200).send(data)
     
   } catch (error) {
