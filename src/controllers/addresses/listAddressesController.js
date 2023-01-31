20d20e3964eb16208026f25b3fdfa3f54b54ce07
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-01-31 11:44:40 -0300

    :boom: fix: fixing to return all addresses

--- src/controllers/addresses/listAddressesController.js
+++ src/controllers/addresses/listAddressesController.js
@@ -7,8 +7,10 @@ export default async (req, res, next) => {
   try {
     listAddressesValidator.parse(req.query)
 
-    const user = await getUser(req.query.userId)
-    if (!user) throw new NotFoundError("This user doesn't exist!", "userId")
+    if (req.query?.userId) {
+      const user = await getUser(req.query.userId)
+      if (!user) throw new NotFoundError("This user doesn't exist!", "userId")
+    }
 
     const data = await listAddresses(req.query)
     if (!data[0]) throw new NotFoundError("This user doesn't have an address!", "userId")
