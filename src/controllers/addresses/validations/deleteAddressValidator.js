ea28478f74f7ea173e80618d4cc763c7b99b8cfd
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-01-31 11:31:42 -0300

    :recycle: refactor: improving validations and throwing custom errors

--- src/controllers/addresses/validations/deleteAddressValidator.js
+++ src/controllers/addresses/validations/deleteAddressValidator.js
@@ -1,4 +1,16 @@
-import zod  from 'zod';
-import { isValidObjectId } from '../../../utils/validations.js';
+import deleteAddressValidatorSchema from "./schemas/deleteAddressValidatorSchema.js";
+import { getAddress } from "../../../services/address.js";
+import { NotFoundError } from "../../../utils/errorHandler.js";
 
-export default zod.string().refine( isValidObjectId, {message: "Invalid ID!"});
+export const validate = async (params) => {
+  try {
+    deleteAddressValidatorSchema.parse(params.id)
+
+    const address = await getAddress(params.id)
+    if (!address) throw new NotFoundError("The ID of this address doesn't exist!", "id")
+    
+  } catch (error) {
+    throw error
+
+  }
+}
