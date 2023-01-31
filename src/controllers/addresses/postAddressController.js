504fbc2f9a669e4c9122e1a9f062f73345d2c15f
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-01-31 11:36:35 -0300

    :sparkles: feat: updating the user's addressIds field when registering the address

--- src/controllers/addresses/postAddressController.js
+++ src/controllers/addresses/postAddressController.js
@@ -1,5 +1,6 @@
 import { postAddress } from "../../services/address.js";
 import postAddressValidator from "./validations/postAddressValidator.js";
+import { getUser, updateUser } from "../../services/users.js";
 
 
 export default async (req, res, next) => { 
@@ -9,6 +10,15 @@ export default async (req, res, next) => {
     postAddressValidator.parse(req.body)
 
     const data = await postAddress(req.body)
+
+    const user = await getUser(req.body.userId, "-password")
+
+    const address = {
+      addressIds: [...user.addressIds, data._id]
+    }
+
+    const addAddress = await updateUser(req.body.userId, address)
+
     res.status(200).send(data)
 
   } catch (error) {
