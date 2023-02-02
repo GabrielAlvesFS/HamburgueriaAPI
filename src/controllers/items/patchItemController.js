229d64fde8f67d6e9054300e8a3dc415de0af1fa
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-02-02 11:33:03 -0300

    :sparkles: feat: returning updated item

--- src/controllers/items/patchItemController.js
+++ src/controllers/items/patchItemController.js
@@ -1,5 +1,5 @@
 import { validate } from "./validators/patchItemValidator.js";
-import { updateItem } from "../../services/items.js";
+import { getItem, updateItem } from "../../services/items.js";
 
 export default async (req, res, next) => {
   try {
@@ -10,7 +10,9 @@ export default async (req, res, next) => {
     if (req.body.id) delete req.body.id
 
     const data = await updateItem(req.params.id, req.body)
-    res.status(200).send(data)
+    const itemUpdated = await getItem(req.params.id)
+
+    res.status(200).send(itemUpdated)
     
   } catch (error) {
     // Throwing to error handler 
