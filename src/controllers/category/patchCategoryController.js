05b14021e4d3d621f2fff4a8d5d64811f7b2029c
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-02-02 11:24:25 -0300

    :sparkles: feat: returning updated category

--- src/controllers/category/patchCategoryController.js
+++ src/controllers/category/patchCategoryController.js
@@ -1,5 +1,5 @@
 import { validate } from "./validations/patchCategoryValidator.js";
-import { updateCategory } from "../../services/category.js";
+import { getCategory, updateCategory } from "../../services/category.js";
 
 export default async (req, res, next) => {
   try {
@@ -10,7 +10,9 @@ export default async (req, res, next) => {
     if (req.body.id) delete req.body.id
 
     const data = await updateCategory(req.params.id, req.body)
-    res.status(200).send(data)
+    const categoryUpdated = await getCategory(req.params.id)
+
+    res.status(200).send(categoryUpdated)
 
   } catch (error) { 
     // Throwing to error handler
