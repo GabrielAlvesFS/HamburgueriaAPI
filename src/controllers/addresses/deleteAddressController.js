389dd3e3a67961a6d46e9bbd24defa1fb5baf9ad
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-02-06 12:03:33 -0300

    :boom: fix: adapting the function so that only the user can make the request

--- src/controllers/addresses/deleteAddressController.js
+++ src/controllers/addresses/deleteAddressController.js
@@ -6,19 +6,18 @@ export default async (req, res, next) => {
   try {
     await validate(req.params)
 
-    const data = await deleteAddress({
-      _id: req.params.id
-    })
-
+    const data = await deleteAddress(req.params.id, req.payload.id)
     const user = await getUser(req.payload.id, "-password")
     
-    const address = user.addressIds.filter((address) => {
-      if (address !== req.params.id) {
-        return address
-      }
-    })
-
-    const newAddress = await updateUser(req.payload.id, {addressIds: address})
+    if (data.deletedCount === 1) {
+      const address = user.addressIds.filter((address) => {
+        if (address !== req.params.id) {
+          return address
+        }
+      })
+  
+      const newAddress = await updateUser(req.payload.id, {addressIds: address})
+    }
 
     res.status(200).send(data)
     
--- src/controllers/addresses/patchAddressController.js
+++ src/controllers/addresses/patchAddressController.js
@@ -3,18 +3,14 @@ import { getAddress } from "../../services/address.js";
 import patchAddressValidator from "./validations/patchAddressValidator.js";
 import { NotFoundError } from "../../utils/errorHandler.js";
 
-
 export default async (req, res, next) => {
   try {
     patchAddressValidator.parse({...req.params, ...req.body})
+    
+    const updatedAddress = await updateAddress(req.params.id, req.payload.id, req.body)
+    if (!updatedAddress) throw new NotFoundError("The ID of this address doesn't exist or you are trying to update an address who don't belongs you!", "id")
 
-    const address = await getAddress(req.params.id)
-    if (!address) throw new NotFoundError("The ID of this address doesn't exist!", "id")
-
-    const data = await updateAddress(req.params.id, req.body)
-    const addressUpdated = await getAddress(req.params.id)
-
-    res.status(200).send(addressUpdated)
+    res.status(200).send(updatedAddress)
     
   } catch (error) {
     next(error)
