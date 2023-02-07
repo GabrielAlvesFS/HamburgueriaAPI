f425e437177555fe376b56e33b2518a18a830d1f
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-02-07 15:34:15 -0300

    :sparkles: feat: validating addressId

--- src/controllers/orders/validations/schemas/postOrderValidatorSchema.js
+++ src/controllers/orders/validations/schemas/postOrderValidatorSchema.js
@@ -12,32 +12,6 @@ export default zod.object({
         })
       ).optional()
     })
-  )
+  ),
+  addressId: zod.string().refine( isValidObjectId, {message: "Invalid ID!"})
 }).strict()
-
-// Validator do schema
-// {
-//   "items": [
-//     {
-//       "id": "63a9adecdaf7efdf35042535",
-//       "complements": [
-//         {
-//           "id": "63a9ad2aaadc3c13f2b99476",
-//           "items": [
-//             "63a9aae0aadc3c13f2b99457",
-//             "63a9ab2daadc3c13f2b99459"
-//           ]
-//         },
-//         {
-//           "id": "63a9ad2aaadc3c13f2b99476",
-//           "items": [
-//             "63a9aae0aadc3c13f2b99457"
-//           ]
-//         }
-//       ]
-//     },
-//     {
-//       "id": "63b5c1766d82b760148fd5da"
-//     }
-//   ]
-// }
