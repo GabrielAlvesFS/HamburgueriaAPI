e5bae9b0a349874d51c2a887d19bd69e43f7626d
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-02-07 13:38:32 -0300

    :sparkles: feat: adding defaultAddress in validation

--- src/controllers/users/validators/patchUserValidator.js
+++ src/controllers/users/validators/patchUserValidator.js
@@ -1,6 +1,6 @@
 import patchUserValidatorSchema from "./schemas/patchUserValidatorSchema.js";
 import { getUser } from "../../../services/users.js";
-import { NotFoundError } from "../../../utils/errorHandler.js";
+import { InvalidAttributionError, NotFoundError } from "../../../utils/errorHandler.js";
 
 export const validate = async (params, body) => {
   try {
@@ -10,6 +10,13 @@ export const validate = async (params, body) => {
     //Verifying if ID exists
     const user = await getUser(params.id)
     if (!user) throw new NotFoundError("This user doesn't exist!", "id")
+
+    if (body?.defaultAddress) {
+      const containValidAddress = user.addressIds.includes(body.defaultAddress)
+      if (!containValidAddress) throw new InvalidAttributionError(
+        "You cannot add this address by default because it is not valid", { defaultAddress: body.defaultAddress}
+      )
+    }
 
   } catch (error) {
     // Throwing error to controller
--- src/controllers/users/validators/schemas/patchUserValidatorSchema.js
+++ src/controllers/users/validators/schemas/patchUserValidatorSchema.js
@@ -6,4 +6,5 @@ export default zod.object({
   name: zod.string().min(3).max(100).optional(),
   birthDate: zod.preprocess((dateString) => new Date(zod.string(dateString).min(10, { message: "The date string must be at least 10 characters long"}).parse(dateString)), zod.date()).optional(),
   phone: zod.string().optional(),
+  defaultAddress: zod.string().refine( isValidObjectId, {message: "Invalid ID!"} ).optional()
 }).strict()
