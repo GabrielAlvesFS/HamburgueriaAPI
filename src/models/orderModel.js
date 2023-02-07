3716398199a51228efdaa64e87b98f57b3d468fb
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-02-07 15:38:56 -0300

    :sparkles: feat: adding amount and address

--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose;
 const orderSchema = new Schema ({
   userData: {type: Object, required: true},
   items: {type: Array, required: true},
-  amount: {type: Number, required: true}
+  amount: {type: Number, required: true},
+  address: {type: Object, required: true}
 }, {timestamps: true})
 
 const order = mongoose.model('Order', orderSchema)
