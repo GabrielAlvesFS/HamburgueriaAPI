c612d7036caf35ae5eb9e892f648f6bd6e858dc9
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-02-07 15:35:13 -0300

    :sparkles: feat: Adding default address field

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -9,7 +9,8 @@ const userSchema = new Schema({
   cpf: {type: String, unique: true},
   phone: {type: String, required: true},
   addressIds: [ {type: String} ],
-  paymentsId: {type: String},
+  defaultAddress: {type: String},
+  paymentsId: {type: String}
 }, {timestamps: true})
 
 const user = mongoose.model('User', userSchema)
