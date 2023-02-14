6aacffab26afc1816b03307e4059da7b6dc9bc6a
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-02-14 17:32:22 -0300

    ✨ feat: adding delete method into routes

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -4,6 +4,7 @@ import postPayment from '../controllers/payment/postPaymentController.js';
 import listPayments from '../controllers/payment/listPaymentsController.js';
 import getPayment from '../controllers/payment/getPaymentController.js';
 import patchPayment from '../controllers/payment/patchPaymentController.js';
+import deletePayment from '../controllers/payment/deletePaymentController.js';
 
 const router = express.Router()
 
@@ -11,6 +12,7 @@ router
     .post("/v1/payment", checkAuth("manager"), postPayment)
     .get("/v1/payment", checkAuth("customer", "manager"), listPayments)
     .get("/v1/payment/:id", checkAuth("customer", "manager"), getPayment)
-    .patch("/v1/payment/:id", checkAuth("customer", "manager"), patchPayment)
+    .patch("/v1/payment/:id", checkAuth("manager"), patchPayment)
+    .delete("/v1/payment/:id", checkAuth("manager"), deletePayment)
 
 export default router;
