f8c71200e32f8a66ff8275aba0202693588fc308
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-02-14 15:23:12 -0300

    :sparkles: feat: adding payment field

--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -5,7 +5,8 @@ const orderSchema = new Schema ({
   userData: {type: Object, required: true},
   items: {type: Array, required: true},
   amount: {type: Number, required: true},
-  address: {type: Object, required: true}
+  address: {type: Object, required: true},
+  payment: {type: Object, required: true},
 }, {timestamps: true})
 
 const order = mongoose.model('Order', orderSchema)
