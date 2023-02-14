1a8486c27a13a48fb68cdc08503790572bb9bb45
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-02-14 17:08:00 -0300

    ✨ feat: adding patch method into routes

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -3,6 +3,7 @@ import checkAuth from "../middlewares/checkAuth.js"
 import postPayment from '../controllers/payment/postPaymentController.js';
 import listPayments from '../controllers/payment/listPaymentsController.js';
 import getPayment from '../controllers/payment/getPaymentController.js';
+import patchPayment from '../controllers/payment/patchPaymentController.js';
 
 const router = express.Router()
 
@@ -10,5 +11,6 @@ router
     .post("/v1/payment", checkAuth("manager"), postPayment)
     .get("/v1/payment", checkAuth("customer", "manager"), listPayments)
     .get("/v1/payment/:id", checkAuth("customer", "manager"), getPayment)
+    .patch("/v1/payment/:id", checkAuth("customer", "manager"), patchPayment)
 
 export default router;
