54b7f9968c19ff06c2d9f18f121d08aed99af19f
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-02-14 16:39:05 -0300

    ✨ feat: adding get method into routes

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import checkAuth from "../middlewares/checkAuth.js"
 import postPayment from '../controllers/payment/postPaymentController.js';
+import listPayments from '../controllers/payment/listPaymentsController.js';
+import getPayment from '../controllers/payment/getPaymentController.js';
 
 const router = express.Router()
 
 router
     .post("/v1/payment", checkAuth("manager"), postPayment)
+    .get("/v1/payment", checkAuth("customer", "manager"), listPayments)
+    .get("/v1/payment/:id", checkAuth("customer", "manager"), getPayment)
 
 export default router;
