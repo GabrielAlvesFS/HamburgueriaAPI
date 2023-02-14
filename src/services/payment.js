db547e50a5210d57c4c0beb28293174fc80a16b3
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-02-14 16:38:24 -0300

    ✨ feat: adding get method

--- src/services/payment.js
+++ src/services/payment.js
@@ -4,3 +4,12 @@ import paymentModel from "../models/paymentModel.js";
 export const postPayment = async (data) => {
   return await paymentModel.create(data);
 }
+
+// GET
+export const getPayment = async (id) => {
+  return await paymentModel.findById(id)
+}
+
+export const listPayments = async (query) => {
+  return await paymentModel.find(query);
+}
