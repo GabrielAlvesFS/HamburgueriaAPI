68cf3a9b60375cab1823fde8d52f48711604f63d
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-02-27 12:39:06 -0300

    :sparkles: feat: Adding get and patch method

--- src/services/order.js
+++ src/services/order.js
@@ -4,3 +4,17 @@ import orderModel from "../models/orderModel.js";
 export const postOrder = async (data) => {
   return await orderModel.create(data);
 }
+
+// GET
+export const listOrders = async(query) => {
+  return await orderModel.find(query);
+}
+
+export const getOrder = async (id) => {
+  return await orderModel.findById(id);
+}
+
+// PUT AND PATCH
+export const updateOrder = async (id, data) => {
+  return await orderModel.findOneAndUpdate({_id: id}, { $set: {...data}}, {returnDocument: 'after'})
+}
