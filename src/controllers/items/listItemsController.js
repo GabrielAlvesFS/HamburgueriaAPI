b06def8b36782200464db0519aa55c987f690aec
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-03-07 12:10:46 -0300

    :sparkles: feat: changing to boolean active field

--- src/controllers/items/listItemsController.js
+++ src/controllers/items/listItemsController.js
@@ -4,11 +4,12 @@ import { NotFoundError } from "../../utils/errorHandler.js";
 
 export default async (req, res, next) => {
   try {
+    if (req.query.active === "true") req.query.active = true
+    if (req.query.active === "false") req.query.active = false
+    
     // Validation with ZOD -> only filter by type, name or value
     listItemsValidator.parse(req.query)
 
-    if (req.query.active === "true") req.query.active = true
-    if (req.query.active === "false") req.query.active = false
 
     const data = await listItems(req.query);
     if (!data[0]) throw new NotFoundError("Item not found!", [])
