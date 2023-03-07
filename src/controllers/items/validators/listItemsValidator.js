111cfd2d4dbc731cdcf33b01353be1703a7488ee
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-03-07 12:10:18 -0300

    :boom: fix: Fixing validation type of active field

--- src/controllers/items/validators/listItemsValidator.js
+++ src/controllers/items/validators/listItemsValidator.js
@@ -5,7 +5,7 @@ export default zod.object({
   type: zod.string().min(3).max(14).optional(),
   name: zod.string().min(3).max(40).optional(),
   value: zod.string().optional(),
-  active: zod.enum(["true", "false"]).optional(),
+  active: zod.boolean().optional(),
   categoryId: zod.string().refine( isValidObjectId, {message: "Invalid ID!"}).optional(),
   complementsIds: zod.string().refine( isValidObjectId, {message: "Invalid ID!"}).optional()
 }).strict()
