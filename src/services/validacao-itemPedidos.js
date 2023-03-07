673901b3d139f90056123e461e24e5ab02eadb46
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-03-07 13:56:00 -0300

    :fire: Removing files from the old API

--- src/services/validacao-itemPedidos.js
+++
@@ -1,7 +0,0 @@
-export const validaIDs = (id) => {
-    if (!id || isNaN(id)) throw new Error("id inválido: Envie um ID Válido")
-}
-
-export const validaQTD = (qtd) => {
-    if (!qtd || isNaN(qtd)) throw new Error("Quantidade inválida: Envie uma quantidade válida!")
-}
