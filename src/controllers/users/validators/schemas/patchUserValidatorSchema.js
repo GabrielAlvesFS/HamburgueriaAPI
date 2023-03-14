2d7a670908ca65306ef548857a3224be49e1c612
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-03-14 13:49:32 -0300

    :boom: fix: fixing birthdate field

--- src/controllers/users/validators/schemas/patchUserValidatorSchema.js
+++ src/controllers/users/validators/schemas/patchUserValidatorSchema.js
@@ -4,7 +4,7 @@ import { isValidObjectId } from '../../../../utils/validations.js';
 export default zod.object({
   id: zod.string().refine( isValidObjectId, {message: "Invalid ID!"} ),
   name: zod.string().min(3).max(100).optional(),
-  birthDate: zod.preprocess((dateString) => new Date(zod.string(dateString).min(10, { message: "The date string must be at least 10 characters long"}).parse(dateString)), zod.date()).optional(),
+  birthdate: zod.preprocess((dateString) => new Date(zod.string(dateString).min(10, { message: "The date string must be at least 10 characters long"}).parse(dateString)), zod.date()).optional(),
   phone: zod.string().optional(),
   defaultAddress: zod.string().refine( isValidObjectId, {message: "Invalid ID!"} ).optional()
 }).strict()
