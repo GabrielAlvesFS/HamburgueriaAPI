b23fac35c56b80f712f38551d4ef75fe120571c3
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-03-14 13:55:01 -0300

    :sparkles: feat: deleting user addresses

--- src/controllers/users/deleteUserController.js
+++ src/controllers/users/deleteUserController.js
@@ -1,15 +1,24 @@
 import { validate } from "./validators/deleteUserValidator.js";
 import { deleteUser } from "../../services/users.js";
+import { deleteAddress } from "../../services/address.js";
 
 export default async (req, res, next) => {
   try {
     //Validation
-    await validate(req.params)
+    const userAfterValidated = await validate(req.params)
 
     if (req.payload?.id) {
       if (req.params.id !== req.payload.id) return res.status(401).send({message: "unathorized"})
     }
     
+    if (userAfterValidated?.addressIds) {
+      await Promise.all(
+        userAfterValidated.addressIds.map(async currAddressId => {
+          await deleteAddress(currAddressId, req.payload.id)
+        })
+      )
+    }
+
     //If ID exists, the user will be deleted
     const data = await deleteUser({
       _id: req.params.id
