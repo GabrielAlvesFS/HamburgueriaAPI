23ee69cfc1580ff0d6307f74d3af924e5b0f7f43
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-03-14 13:48:59 -0300

    :boom: fix: fixing birthdate field

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
   name: {type: String, required: true},
   email: {type: String, required: true, unique: true},
   password: {type: String, required: true},
-  birthDate: {type: Date, required: true},
+  birthdate: {type: Date, required: true},
   cpf: {type: String, unique: true},
   phone: {type: String, required: true},
   addressIds: [ {type: String} ],
