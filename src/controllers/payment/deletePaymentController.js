9b50977b5bce6e96e0783d32a783c7b11efd3ab7
GabrielAlvesFS <Gabriel_Alves64@hotmail.com>
2023-03-20 11:34:12 -0300

    :sparkles: feat: verifying if payment type exists

--- src/controllers/payment/deletePaymentController.js
+++ src/controllers/payment/deletePaymentController.js
@@ -1,9 +1,13 @@
-import { deletePayment } from "../../services/payment.js";
+import { deletePayment, getPayment } from "../../services/payment.js";
 import deletePaymentValidator from "./validators/deletePaymentValidator.js";
+import { NotFoundError } from "../../utils/errorHandler.js";
 
 export default async (req, res, next) => {
   try {
     deletePaymentValidator.parse(req.params.id)
+
+    const payment = await getPayment(req.params.id)
+    if (!payment) throw new NotFoundError("This payment doesn't exist!", "id")
     
     const data = await deletePayment({
       _id: req.params.id
